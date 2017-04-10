b9ce543929472b1d64c0477f21b5ac2d1eb0795f
Marcelo Tokarnia <marcelo.tokarnia@gmail.com>
2017-04-10 18:11:44 -0300

    classmethod to static method
    since i'm not using the cls or the self, it should probably be a staticmethod

--- django_admin_raw_id_fields.py
+++ django_admin_raw_id_fields.py
@@ -32,8 +32,9 @@ class RawIdAdminModel(admin.ModelAdmin):
     def __init__(self, model, admin_site, *args, **kwargs):
         self.raw_id_fields = [field.name for field in model._meta.concrete_fields if self.is_fk_or_m2m(field)]
         super(RawIdAdminModel, self).__init__(model, admin_site, *args, **kwargs)
-        
-    def is_fk_or_m2m(self, field):
+
+    @staticmethod
+    def is_fk_or_m2m(field):
         return reduce((lambda x, y: x or isinstance(field, y)), [models.ForeignKey, models.OneToOneField, models.ManyToManyField], False)
         
 # now instead of doing: admin.site.register(MyClass2, MyClassAdmin), this will have the same effect:
